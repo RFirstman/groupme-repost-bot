662b104201c0efa4a0860ab08e0c44af3a7499cf
Robert Firstman <robfirstman@gmail.com>
2018-07-25 20:58:22 -0400

    Fixed minor bug

--- routes/callbackRoutes.js
+++ routes/callbackRoutes.js
@@ -1,13 +1,13 @@
-var axios = require("axios");
+var curl = require("curlrequest");
 
-const { bot_id } = require("../config/index");
+const { bot_id, group_id } = require("../config/index");
 console.log(bot_id)
 
 module.exports = app => {
     app.post("/callback", (req, res) => {
         console.log(req.body);
 
-        if (req.body.group_id === bot_id) {
+        if (req.body.group_id === group_id) {
             axios.post("https://api.groupme.com/v3/bots/post", {
                 text: "I am repost bot.",
                 bot_id
