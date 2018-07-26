27c75714efc88c79713941d9f5dffd23c3b94149
Robert Firstman <robfirstman@gmail.com>
2018-07-25 21:00:48 -0400

    bugfix

--- routes/callbackRoutes.js
+++ routes/callbackRoutes.js
@@ -2,16 +2,28 @@ var curl = require("curlrequest");
 var axios = require("axios");
 
 const { bot_id, group_id } = require("../config/index");
-console.log(bot_id)
+console.log(bot_id, group_id)
 
 module.exports = app => {
     app.post("/callback", (req, res) => {
         console.log(req.body);
 
         if (req.body.group_id === group_id) {
-            axios.post("https://api.groupme.com/v3/bots/post", {
+            console.log("sending message")
+            /* axios.post("https://api.groupme.com/v3/bots/post", {
                 text: "I am repost bot.",
                 bot_id
+            }); */
+            var options = {
+                method: "POST",
+                url: "https://api.groupme.com/v3/bots/post",
+                data: { text: "hello!", bot_id }
+            };
+    
+            curl.request(options, function(error, response) {
+                if (error) {
+                    console.log(error);
+                }
             });
         }
         res.sendStatus(200);
